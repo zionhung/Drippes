f64ac4411778af779d5910936cf63dbf8783a5fd
Rolando Lopantzi <rlopantzi@gmail.com>
2020-01-30 11:42:16 -0800

    init commit
    I have been styling the homepage for the site

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  {path: '', pathMatch:}
+  {path: '', pathMatch:'full', redirectTo: '/drippes'},
+  {path: 'drippes', component: HomeComponent},
 ];
 
 @NgModule({
