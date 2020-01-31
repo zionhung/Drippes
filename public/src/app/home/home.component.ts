a7d4e874388b40c45619500718e7de664e756ef0
Rolando Lopantzi <rlopantzi@gmail.com>
2020-01-30 17:30:40 -0800

    Update home.component.ts

--- public/src/app/home/home.component.ts
+++ public/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from "../http.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  products = [];
+  selectedProduct = {};
+  constructor(private _http: HttpService) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
+  
 }
